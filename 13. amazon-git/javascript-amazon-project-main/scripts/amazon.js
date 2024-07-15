9354edb893beea684ce314254268b7cba95178ec
rank1dave <addo.david32@gmail.com>
2024-07-15 11:26:49 -0500

    Made the quantity selector interactive

--- 13. amazon-git/javascript-amazon-project-main/scripts/amazon.js
+++ 13. amazon-git/javascript-amazon-project-main/scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
       <div class="product-price">${(product.priceCents / 100).toFixed(2)}</div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -67,10 +67,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 			}
 		});
 
+		const quantitySelector = document.querySelector(
+			`.js-quantity-selector-${productId}`
+		);
+		const quantity = Number(quantitySelector.value);
+
 		if (matchingItem) {
-			matchingItem.quantity++;
+			matchingItem.quantity += quantity;
 		} else {
-			cart.push({ productId: productId, quantity: 1 });
+			cart.push({ productId: productId, quantity: quantity });
 		}
 
 		let cartQuantity = 0;
@@ -78,5 +83,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 			cartQuantity += item.quantity;
 		});
 		document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+		console.log(cart);
 	});
 });
